7be946b74fc64be4a567e499867acdf0f0b33a52
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-11-15 11:46:51 -0600

    added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO employees (employee_name)
+VALUES ('Jeff McLeod'),
+       ('Abby Heartfelt'),
+       ('Bobby Carter'),
+       ('Ramee El Ramen'),
+       ('Yuno Sykk'),
+       ('Murphy Braun'),
+       ('Randy Bullet');
+
+INSERT INTO departments (employee_id, department)
+VALUES (1, 'Sales'),
+       (2, 'Engineering'),
+       (3, 'Finance'),
+       (4, 'Legal');
+
+INSERT INTO roles (employee_id, title)
+VALUES (1, 'Salesperson'),
+       (2, 'Software Engineer'),
+       (3, 'Account Manager'),
+       (4, 'Accountant'),
+       (5, 'Legal Team Lead'),
+       (6, 'Lawyer');
