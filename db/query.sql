2e33de5b5b7f22533883c3b3038a29f0a0ca5003
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-11-15 13:36:25 -0600

    created mysql connection added query

--- db/query.sql
+++
@@ -1,5 +0,0 @@
-SELECT employees.department AS departments, departments.department 
-FROM departments
-LEFT JOIN employees
-ON departments.employees_id = employees.id
-ORDER BY employees.employee_name;
--- server.js
+++ server.js
@@ -1,68 +1,23 @@
-const express = require('express');
-const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const inquirer = require('inquirer');
 require('console.table');
-
-// middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      // MySQL username,
       user: 'root',
-      // TODO: Add MySQL password here
       password: '',
       database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
 );
 
-// Create a new employee
-app.post('/api/new-employee', ({ body }, res) => {
-    const sql = `INSERT INTO employee (employee_name)
-      VALUES (?)`;
-    const params = [body.employee_name];
-    
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
-    });
-});
+db.query(
+    'SELECT * FROM departments',
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    },
+);
 
-// See all employees
-app.get('/api/employees', (req, res) => {
-    const sql = `SELECT id, employee_name AS employee_name FROM employees`;
-    
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-         return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-  });
-
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
-  
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
-});
-  
